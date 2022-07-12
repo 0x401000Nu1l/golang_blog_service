b7af08b0eb777cb34d48c3932b93e03b2413e192
0x401000Nu1l <xindu1106@gamil.com>
2022-07-12 21:38:36 +0800

    完成了项目标准目录创建，数据库设计，数据模型编写，接口方法设计，接口处理方法

---
+++ internal/model/article.go
@@ -0,0 +1,14 @@
+package model
+
+type Articles struct {
+	*Model
+	Title         string `json:"title"`
+	Desc          string `json:"desc"`
+	Content       string `json:"content"`
+	CoverImageUrl string `json:"cover_image_url"`
+	State         uint8  `json:"state"`
+}
+
+func (a Articles) TableName() string {
+	return "blog_article"
+}
